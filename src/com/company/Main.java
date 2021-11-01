d92345468580659322d9baf81cd0e9f7e18afd72
wiwi081 <aleksei5555578@mail.ru>
2021-11-01 21:54:15 +0300

    added calc

---
+++ src/com/company/Main.java
@@ -0,0 +1,90 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class Main {
+
+    public static void main(String[] args) {
+        int num1, num2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите 1 число");
+
+        num1 = scanner.nextInt();
+        System.out.print("Введите 2 число");
+        num2 = scanner.nextInt();
+
+        System.out.print("Выберете оператора (+, -, *, /)= ");
+        char operator = scanner.next().charAt(0);
+
+        scanner.close();
+        int output;
+
+        switch (operator) {
+            case '+':
+                output = num1 + num2;
+                break;
+
+            case '-':
+                output = num1 - num2;
+                break;
+
+            case '*':
+                output = num1 * num2;
+                break;
+
+            case '/':
+                output = num1 / num2;
+                break;
+
+            default:
+                System.out.printf("Выберете нужный оператор");
+                return;
+        }
+
+        System.out.println(num1 + " " + operator + " " + num2 + "= " + output);
+    }
+
+    private static String convertNumToRoman(int numArabian) {
+        String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+        };
+        final String s = roman[numArabian];
+        return s;
+    }
+
+
+    private static int romanToNumber(String roman) {
+        try {
+            if (roman.equals("I")) {
+                return 1;
+            } else if (roman.equals("II")) {
+                return 2;
+            } else if (roman.equals("III")) {
+                return 3;
+            } else if (roman.equals("IV")) {
+                return 4;
+            } else if (roman.equals("V")) {
+                return 5;
+            } else if (roman.equals("VI")) {
+                return 6;
+            } else if (roman.equals("VII")) {
+                return 7;
+            } else if (roman.equals("VIII")) {
+                return 8;
+            } else if (roman.equals("IX")) {
+                return 9;
+            } else if (roman.equals("X")) {
+                return 10;
+            }
+        } catch (InputMismatchException e) {
+            throw new InputMismatchException("Неверный формат данных");
+        }
+        return -1;
+    }
+}
+
